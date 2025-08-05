201797742cb5f28c0e5ed93d2a30005d8f5a9d51
BRUNA FIUSA <bruna.fiusa@senacsp.edu.br>
2025-08-05 17:36:32 -0300

    primeiro commit

---
+++ src/components/LoginForm.js
@@ -0,0 +1,24 @@
+export default function LoginForm() {
+    const formulario = document.createElement('form');
+    
+    const email = document.createElement('input');
+    email.type = 'email';
+    email.placeholder = "Digite seu e-mail";
+    email.className = 'input';
+    formulario.appendChild(email);
+
+    const senha = document.createElement('input');
+    senha.type = 'password'; 
+    senha.placeholder = "Digite sua senha";
+    senha.className = 'input';
+    formulario.appendChild(senha);
+
+    const btnAut = document.createElement('button');
+    btnAut.type = 'submit';
+    btnAut.textContent = "Entrar";
+    btnAut.className = 'input';
+
+    formulario.appendChild(btnAut);
+
+    return formulario;
+}
