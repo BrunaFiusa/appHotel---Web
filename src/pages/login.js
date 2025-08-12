cf32a15557d59b5ed1e0b1ffb344653d4334d614
BRUNA FIUSA <bruna.fiusa@senacsp.edu.br>
2025-08-12 17:23:15 -0300

    logo adicionada

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,13 @@
 import LoginForm from "../components/LoginForm.js";
+import NavBar from "../components/Navbar.js";
 
 export default function renderLoginPage() { 
+    const nav = document.getElementById('navbar');
+    nav.innerHTML = '';
+
+    const navbar = NavBar();
+    nav.appendChild(navbar);
+
     const divRoot = document.getElementById('root');
     divRoot.innerHTML = '';
 
