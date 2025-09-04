d9593726bf7819f01edda7eaf207e37da07756c8
BRUNA FIUSA <bruna.fiusa@senacsp.edu.br>
2025-09-04 17:17:28 -0300

    criando a classe QuartosController e atualizando a classe QuartosModel

--- teste.php
+++ teste.php
@@ -1,20 +1,26 @@
 <?php
     require_once __DIR__ ."/controllers/AutController.php";
+    require_once __DIR__ ."/controllers/QuartosController.php";
     require_once __DIR__ ."/controllers/PasswordController.php";
     require_once __DIR__ ."/helpers/token_jwt.php";
     
     $data = [
-        "email" => "osanaam@gmail.com",
-        "senha" => "121274"
+        "nome" => "Quarto Supremo",
+        "numero" => 1,
+        "qtd_cama_casal" => 2,
+        "qtd_cama_solteiro" => 0,
+        "preco" => 2500,
+        "disponivel" => 1
     ];
 
+    // QuartosController::create($conn, $data);
+    QuartosController::update($conn, 5, $data);
+
+    //$data = [ "email" => "osanaam@gmail.com", "senha" => "121274"];
+
     //AutController::login($conn, $data);
-
-    $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNZXVTaXRlIiwiaWF0IjoxNzU2OTMwMTE5LCJleHAiOjE3NTY5MzM3MTksInN1YiI6eyJpZCI6Niwibm9tZSI6Ik9zYW5hIiwiZW1haWwiOiJvc2FuYWFtQGdtYWlsLmNvbSIsImNhcmdvIjoiR2VyZW50ZSJ9fQ.Yxm4ZKAoQTlswo6jW_DHMbFlUa-BYs5FsmhCuLlsLDk";
-    echo var_dump( validateToken($token));
-
-    
-
+    //$token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJNZXVTaXRlIiwiaWF0IjoxNzU2OTMwMTE5LCJleHAiOjE3NTY5MzM3MTksInN1YiI6eyJpZCI6Niwibm9tZSI6Ik9zYW5hIiwiZW1haWwiOiJvc2FuYWFtQGdtYWlsLmNvbSIsImNhcmdvIjoiR2VyZW50ZSJ9fQ.Yxm4ZKAoQTlswo6jW_DHMbFlUa-BYs5FsmhCuLlsLDk";
+    //echo var_dump( validateToken($token));
     // echo PasswordController::generateHash($data['senha']);
     //$hash = '$2y$10$zVUF94elzqw1oGIh.dtL6Og5fHv/x/EFEeAtMQ6ncxg/o8mrvNcDu';
     //echo "<br>";
