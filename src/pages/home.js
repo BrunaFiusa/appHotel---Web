c5b131cba15c69413a82b0acb3f2a46e77aa3cad
BRUNA FIUSA <bruna.fiusa@senacsp.edu.br>
2025-10-14 17:16:18 -0300

    alterações no filtro para buscar quartos disponiveis

--- src/pages/home.js
+++ src/pages/home.js
@@ -17,12 +17,10 @@ export default function renderHomePage() {
     const divRoot = document.getElementById('root');
     divRoot.innerHTML = '';
 
-    // const grid = Grid();
-    // grid.style.marginTop = '10%';
-    // divRoot.appendChild(grid);
-
     const hero = Hero();
     divRoot.appendChild(hero);
+
+    // CRIAÇÃO DO SELETOR PARA OS QUARTOS //
 
     const selector = DataSelector();
     divRoot.appendChild(selector);
@@ -30,12 +28,65 @@ export default function renderHomePage() {
     /*Criar uma constante que armazena o valor da data de hoje*/
     const dateToday = new Date().toISOString().split("T")[0];
 
-    const [dateChekin, dateCheckout] = selector.querySelectorAll('input[type="date"]');
-    dateChekin.min = dateToday;
+    const [dateCheckIn, dateCheckout] = selector.querySelectorAll('input[type="date"]');
+    dateCheckIn.min = dateToday;
     dateCheckout.min = dateToday;
 
     const qtdHospedes = selector.querySelector('select');
     const btnDateSelec = selector.querySelector('button');
+
+    function getMinDateCheckout(dateCheckIn) {
+        const minDaily = new Date(dateCheckIn);
+        minDaily.setDate(minDaily.getDate() + 1);
+        return minDaily.toISOString().split('T')[0];
+    }
+
+    /*Evento para monitorar a alteração da data de check-in para mudar a data da check-out*/
+    dateCheckIn.addEventListener("change", async (e) => {
+        if (dateCheckIn.value){
+            const minDateCheckout = getMinDateCheckout(dateCheckIn.value);
+            dateCheckout.min = minDateCheckout;
+        }
+        //se já houbver uma data de checkout selecionada e que seja inválida
+            if(dateCheckout.value && dateCheckout.value <= dateCheckIn.value) {
+                dateCheckout.value = "";
+                alert("A data do check-out tem que ser maior que a de check-in"); //fazer modal pra isso
+            }
+    });
+
+    dateCheckout.addEventListener("change", async (e) => {
+        if (dateCheckIn.value && dateCheckout.value){
+            const checkInValue = new Date(dateCheckIn.value);
+            const checkOutValue = new Date(dateCheckout.value);
+            
+            if (checkOutValue <= checkInValue) {
+                dateCheckout.value = "";
+                alert("A data de check-out deve ser posterior ao check-in!");
+            }
+        }
+    });
+ 
+    btnDateSelec.addEventListener("click", async (evento) =>{
+        evento.preventDefault();
+        const inicio = (dateCheckIn?.value || "").trim();
+        const fim = (dateCheckout?.value || "").trim();
+        const qtd = parseInt(qtdHospedes?.value || "0", 10);
+        try{
+            const quartos = await listAvaibleQuartosRequest({inicio, fim, qtd});
+            if ( ! quartos.length) {
+                return;
+            }
+            divCard.innerHTML = '';
+            quartos.forEach((itemCard, i) => {
+                divCard.appendChild(RoomCard(itemCard, i));
+            });
+            //Após intervalo: prencher as infos dos quartos nos cards ou avisar ao cliente que nao há quarto disponivel
+        }catch(erro){
+            console.log(erro);
+        }
+    });
+
+    // CRIAÇÃO DOS CARDS SOBRE O HOTEL //
 
     const divCard = document.createElement('div');
     divCard.className ="divCard";
@@ -58,41 +109,6 @@ export default function renderHomePage() {
         const cardLounge = CardLounge(lougeItems[i], i);
         cardGroupInfra.appendChild(cardLounge);
     }
-
-    function getMinDateCheckout(dateCheckIn) {
-        const minDaily = new Date(dateCheckIn);
-        minDaily.setDate(minDaily.getDate() + 1);
-        return minDaily.toISOString().split('T')[0];
-    }
-
-    /*Evento para monitorar a alteração da data de check-in para mudar a data da check-out*/
-    dateChekin.addEventListener("change", async (e) => {
-        if (this.value){
-            const minDateCheckout = getMinDateCheckout(this.value);
-            dateCheckout.min = minDateCheckout;
-        }
-    });
-
- 
-    btnDateSelec.addEventListener("click", async (evento) =>{
-        evento.preventDefault();
-        const inicio = (dateChekin?.value || "").trim();
-        const fim = (dateCheckout?.value || "").trim();
-        const qtd = parseInt(qtdHospedes?.value || "0", 10);
-        try{
-            const quartos = await listAvaibleQuartosRequest({inicio, fim, qtd});
-            if ( ! quartos.length) {
-                return;
-            }
-            divCard.innerHTML = '';
-            quartos.forEach((itemCard, i) => {
-                divCard.appendChild(RoomCard(itemCard, i));
-            });
-            //Após intervalo: prencher as infos dos quartos nos cards ou avisar ao cliente que nao há quarto disponivel
-        }catch(erro){
-            console.log(erro);
-        }
-    });
 
     divRoot.appendChild(divCard);
     divRoot.appendChild(tituloInfra);
